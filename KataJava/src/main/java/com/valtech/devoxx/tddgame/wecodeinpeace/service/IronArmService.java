1ad1a20946436019daa577ca78dcfa4a0e60a7d9
thomas.bonset <thomas.bonset@PAR-LAP-1219.valtech.com>
2013-03-25 20:21:46 +0100

    solution 6 : ironarm

--- KataJava/src/main/java/com/valtech/devoxx/tddgame/wecodeinpeace/service/IronArmService.java
+++ KataJava/src/main/java/com/valtech/devoxx/tddgame/wecodeinpeace/service/IronArmService.java
@@ -7,7 +7,11 @@ public class IronArmService implements IIronArmService {
 	JavaltechianService service = new JavaltechianService();
 	
 	public Javaltechian getWinnerOfTwoJavaltechians(Javaltechian player1, Javaltechian player2) {
-		//TODO improve me
-		return null;
+		double hypotPlayer1 = Math.hypot(service.getPowerByName(player1.getName()), service.getNumberOfDigitsByForename(player1.getForename()));
+		double hypotPlayer2 = Math.hypot(service.getPowerByName(player2.getName()), service.getNumberOfDigitsByForename(player2.getForename()));	
+		if (hypotPlayer1 >= hypotPlayer2)
+			return player1;
+		else 
+			return player2;
 	}
 }
