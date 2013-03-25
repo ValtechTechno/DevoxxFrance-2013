ea082fd4fd5c5d5bc285af2c4c09a49810db9431
thomas.bonset <thomas.bonset@PAR-LAP-1219.valtech.com>
2013-03-25 19:50:10 +0100

    solution 1 : digitjavaltechian

--- KataJava/src/main/java/com/valtech/devoxx/tddgame/wecodeinpeace/service/JavaltechianService.java
+++ KataJava/src/main/java/com/valtech/devoxx/tddgame/wecodeinpeace/service/JavaltechianService.java
@@ -3,6 +3,36 @@ package com.valtech.devoxx.tddgame.wecodeinpeace.service;
 public class JavaltechianService implements IJavaltechianService{
 	
 	public int getNumberOfDigitsByForename(String name){ 	
-		return 0;	
+		int result=0;
+		int b = name.length();
+		while(b > 1){
+			for(int i = 2; i < b+1; i++){
+				if(isPrimeNumber(i)){
+					if(b%i == 0){
+						result++;
+						b = b/i;
+						i = b+1;
+					}
+				}
+			}
+		}
+		return result;
+	}
+
+	private  boolean isPrimeNumber (int x){
+		boolean prime=true;
+		if (x%2==0 && x!=2 || x == 1){
+			prime = false;
+		}
+		else{
+			for(int i=3; i<=Math.sqrt(x); i+=2){
+				if(x%i==0){
+					prime=false;
+				}
+				else 
+					prime=true;
+			}
+		}
+		return prime;
 	}
 }
