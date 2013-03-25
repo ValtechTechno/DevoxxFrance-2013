09aba90fb7a627dcc3333fbc36f49e9ae2a21506
thomas.bonset <thomas.bonset@PAR-LAP-1219.valtech.com>
2013-03-25 20:05:04 +0100

    challenge 3 : spaceshipcapacity

--- KataJava/src/main/java/com/valtech/devoxx/tddgame/wecodeinpeace/service/SpaceshipService.java
+++ KataJava/src/main/java/com/valtech/devoxx/tddgame/wecodeinpeace/service/SpaceshipService.java
@@ -3,7 +3,22 @@ package com.valtech.devoxx.tddgame.wecodeinpeace.service;
 public class SpaceshipService implements ISpaceshipService {
 	
 	public int getMaxCapacityByName(String name){ 
-		//TODO improve me
-		return 0;
+		if (name.toLowerCase().startsWith("bo"))
+			 return 8*10;
+		if (name.toLowerCase().startsWith("lo"))
+			 return 64*10;
+		if (name.toLowerCase().startsWith("in"))
+			 return 32*10;
+		if (name.toLowerCase().startsWith("do"))
+			 return 64*10;
+		if (name.toLowerCase().startsWith("fl"))
+			 return 32*10;
+		if (name.toLowerCase().startsWith("ch"))
+			 return 16*10;
+		if (name.toLowerCase().startsWith("by"))
+			 return 8*10;
+		if (name.toLowerCase().startsWith("sh"))
+			 return 16*10;
+		else return 10;
 	}
 }
